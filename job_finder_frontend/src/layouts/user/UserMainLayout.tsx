24ccabc35c46a46afe0e2a52f0ca3dae67d257c7
kaung san <kaungsan24901@gmail.com>
2025-06-29 19:46:43 +0630

    commit before merge

--- job_finder_frontend/src/layouts/user/UserMainLayout.tsx
+++ job_finder_frontend/src/layouts/user/UserMainLayout.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Header from "../../components/user/Header";
 import { Outlet } from "react-router";
 import AppDrawer from "../../components/user/AppDrawer";
+import Footer from "../../components/user/Footer";
 
 export default function UserMainLayout() {
   return (
@@ -9,6 +10,7 @@ export default function UserMainLayout() {
       <Header />
       <AppDrawer />
       <Outlet />
+      <Footer />
     </Box>
   );
 }
