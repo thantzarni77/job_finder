050d0cbfa9a0fa6d440fd6312ea921c6c643945b
AyeChanMon-coder <ayechanmon011@gmail.com>
2025-07-13 13:56:07 +0630

    talent CRUD

--- job_finder_backend/app/Interfaces/TalentRepositoryInterface.php
+++ job_finder_backend/app/Interfaces/TalentRepositoryInterface.php
@@ -4,5 +4,8 @@ namespace App\Interfaces;
 
 interface TalentRepositoryInterface
 {
-    //
+    public function index();
+    public function create(array $data);
+    public function show($id);
+    public function update(array $data, $id);
 }
--- job_finder_backend/database/seeders/DatabaseSeeder.php
+++ job_finder_backend/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\TypeSeeder;
+use Database\Seeders\TalentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AuthSeeder::class,
-            TypeSeeder::class
+            TypeSeeder::class,
+            TalentSeeder::class
         ]);
     }
 }
