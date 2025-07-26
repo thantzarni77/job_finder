b6502b017e1ad45dce685d74ea14a7644dcdd808
zinwinhtun <zinwinhtun71@gmail.com>
2025-07-26 20:50:16 +0630

    error fixed

--- job_finder_frontend/src/helper/seekerProjectApiFunctions.ts
+++ job_finder_frontend/src/helper/seekerProjectApiFunctions.ts
@@ -1,6 +1,11 @@
 import axiosClient from "./axiosClient";
 
-export const getSeekerProjects = async () => {
-  const { data } = await axiosClient.get("/project");
+export const seekerGetProject = async (seekerId: string | number) => {
+  const { data } = await axiosClient.get(`/project/${seekerId}`);
   return data;
+};
+
+export const seekerAddProject = async (data: FormData) => {
+  const response = await axiosClient.post("/project", data);
+  return response;
 };
