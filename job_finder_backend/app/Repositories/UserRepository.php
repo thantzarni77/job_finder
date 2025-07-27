96ca78eea6526b606b17f7c2e5998a1d6a719b14
thantzarni77 <thantzarni83@gmail.com>
2025-07-27 15:31:31 +0630

    userUpdate function add

--- job_finder_backend/app/Repositories/UserRepository.php
+++ job_finder_backend/app/Repositories/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 use App\Http\Resources\UserResource;
 use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\File;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserRepository implements UserRepositoryInterface
@@ -18,11 +19,30 @@ class UserRepository implements UserRepositoryInterface
     public function updateUser($request, $id)
     {
         $validatedData = $request->validate([
-            "name"    => "required",
-            "email"   => "required",
-            'phone'   => "nullable",
-            'address' => "nullable",
+            "name"            => "required",
+            "email"           => "required",
+            'phone'           => "nullable",
+            'address'         => "nullable",
+            "profile_picture" => "image|mimes:jpeg,png,jpg,gif,svg",
         ]);
+
+        $user = User::findorFail($id);
+        if ($request->hasFile('profile_picture')) {
+            $path = $user->profile_picture;
+
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+        }
+
+        if (file_exists($request['profile_picture'])) {
+            $file         = $request['profile_picture'];
+            $fname        = $file->getClientOriginalName();
+            $imagenewname = uniqid($id) . $id . $fname;
+            $file->move(public_path('assets/img/user-profile/'), $imagenewname);
+            $filepath                         = 'assets/img/user-profile/' . $imagenewname;
+            $validatedData['profile_picture'] = $filepath;
+        }
 
         $updatedUser = User::where('id', $id)->update($validatedData);
 
