6b12aafd85994402f5c5876894a583c977a5ec34
aungkyawzaww <aungkyawzaww7889@gmail.com>
2025-07-28 18:36:52 +0630

    refactor: api route update

--- job_finder_backend/database/seeders/DatabaseSeeder.php
+++ job_finder_backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::factory(10)->create();
         $this->call([
             TypeSeeder::class,
             TalentSeeder::class,
